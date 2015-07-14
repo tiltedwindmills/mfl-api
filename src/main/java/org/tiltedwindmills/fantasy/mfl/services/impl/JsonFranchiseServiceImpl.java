f1d4646fabc0939f4976e9bd8f2f07b5da1aa375
John Daniel <johndaniel@gmail.com>
2015-07-13 23:56:48 -0400

    Fixing some javadoc errors

--- src/main/java/org/tiltedwindmills/fantasy/mfl/services/impl/JsonFranchiseServiceImpl.java
+++ src/main/java/org/tiltedwindmills/fantasy/mfl/services/impl/JsonFranchiseServiceImpl.java
@@ -8,47 +8,47 @@ import org.tiltedwindmills.fantasy.mfl.model.rosters.RostersResponse;
 import org.tiltedwindmills.fantasy.mfl.services.FranchiseService;
 
 /**
- * JSON implementation of the {@link LeagueService}
+ * JSON implementation of the {@link org.tiltedwindmills.fantasy.mfl.services.LeagueService}
  */
 @Service
 public final class JsonFranchiseServiceImpl extends AbstractJsonServiceImpl implements FranchiseService {
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.jwdaniel.services.FranchiseService#getAllRosters(int, java.lang.String, int)
-	 */
-	@Override
-	public List<RosterFranchise> getAllRosters(final int mflLeagueId, final String serverId, final int year) {
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.jwdaniel.services.FranchiseService#getAllRosters(int, java.lang.String, int)
+     */
+    @Override
+    public List<RosterFranchise> getAllRosters(final int mflLeagueId, final String serverId, final int year) {
 
-		final MflFranchiseExport franchiseExport = getRestAdapter(serverId).create(MflFranchiseExport.class);
-		final RostersResponse rostersResponse = franchiseExport.getRoster(mflLeagueId, null, year);
+        final MflFranchiseExport franchiseExport = getRestAdapter(serverId).create(MflFranchiseExport.class);
+        final RostersResponse rostersResponse = franchiseExport.getRoster(mflLeagueId, null, year);
 
-		if (rostersResponse != null && rostersResponse.getWrapper() != null) {
-			return rostersResponse.getWrapper().getFranchiseList();
-		}
+        if (rostersResponse != null && rostersResponse.getWrapper() != null) {
+            return rostersResponse.getWrapper().getFranchiseList();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.jwdaniel.services.FranchiseService#getFranchiseRoster(java.lang.String, int, java.lang.String, int)
-	 */
-	@Override
-	public RosterFranchise getFranchiseRoster(final String franchise,
-											final int mflLeagueId,
-											final String serverId,
-											final int year) {
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.jwdaniel.services.FranchiseService#getFranchiseRoster(java.lang.String, int, java.lang.String, int)
+     */
+    @Override
+    public RosterFranchise getFranchiseRoster(final String franchise,
+                                            final int mflLeagueId,
+                                            final String serverId,
+                                            final int year) {
 
-		final MflFranchiseExport franchiseExport = getRestAdapter(serverId).create(MflFranchiseExport.class);
-		final RostersResponse rostersResponse = franchiseExport.getRoster(mflLeagueId, franchise, year);
+        final MflFranchiseExport franchiseExport = getRestAdapter(serverId).create(MflFranchiseExport.class);
+        final RostersResponse rostersResponse = franchiseExport.getRoster(mflLeagueId, franchise, year);
 
-		if (rostersResponse != null && rostersResponse.getWrapper() != null) {
-			return rostersResponse.getWrapper().getFranchiseList().get(0);
-		}
+        if (rostersResponse != null && rostersResponse.getWrapper() != null) {
+            return rostersResponse.getWrapper().getFranchiseList().get(0);
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
