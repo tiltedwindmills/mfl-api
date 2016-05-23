dc78ad185bffef8ab0d58e6299e88ab3281cd9fd
John Daniel <johndaniel@gmail.com>
2016-05-23 00:10:24 -0400

    Enabling year selections for Draft.

--- src/main/java/org/tiltedwindmills/fantasy/mfl/services/LeagueService.java
+++ src/main/java/org/tiltedwindmills/fantasy/mfl/services/LeagueService.java
@@ -14,59 +14,67 @@ import org.tiltedwindmills.fantasy.mfl.model.weeklyresults.WeeklyResultsWrapper;
  */
 public interface LeagueService {
 
-    /**
-     * Search the leagues.
-     *
-     * @param email the email
-     * @param year the year
-     * @return a list of leagues.
-     */
-    List<LeagueSearchResult> searchLeagues(String email, int year);
+	/**
+	 * Search the leagues.
+	 *
+	 * @param email the email
+	 * @param year the year
+	 * @return a list of leagues.
+	 */
+	List<LeagueSearchResult> searchLeagues(String email, int year);
 
-    /**
-     * Gets the league.
-     *
-     * @param mflLeagueId the mfl league id
-     * @param serverId the server id
-     * @return the league
-     */
-    League getLeague(int mflLeagueId, String serverId);
+	/**
+	 * Gets the league.
+	 *
+	 * @param mflLeagueId the mfl league id
+	 * @param serverId the server id
+	 * @return the league
+	 */
+	League getLeague(int mflLeagueId, String serverId);
 
-    /**
-     * Gets the league.
-     *
-     * @param mflLeagueId the mfl league id
-     * @param franchiseId the franchise id
-     * @param password the password
-     * @param serverId the server id
-     * @return the league
-     */
-    League getLeague(int mflLeagueId, java.lang.String franchiseId, String password, String serverId);
+	/**
+	 * Gets the league.
+	 *
+	 * @param mflLeagueId the mfl league id
+	 * @param franchiseId the franchise id
+	 * @param password the password
+	 * @param serverId the server id
+	 * @return the league
+	 */
+	League getLeague(int mflLeagueId, java.lang.String franchiseId, String password, String serverId);
 
-    /**
-     * Gets the live scoring.
-     *
-     * @param mflLeagueId the mfl league id
-     * @param week the week
-     * @param serverId the server id
-     * @return the live scoring
-     */
-    LiveScoringWrapper getLiveScoring(int mflLeagueId, int week, String serverId);
-
-
-
-    WeeklyResultsWrapper getWeeklyResults(int mflLeagueId, int week, String serverId, int year);
-
-    /**
-     * Gets the standings.
-     *
-     * @param mflLeagueId the mfl league id
-     * @param serverId the server id
-     * @param year the year
-     * @return the standings
-     */
-    List<Standings> getStandings(int mflLeagueId, String serverId, int year);
+	/**
+	 * Gets the live scoring.
+	 *
+	 * @param mflLeagueId the mfl league id
+	 * @param week the week
+	 * @param serverId the server id
+	 * @return the live scoring
+	 */
+	LiveScoringWrapper getLiveScoring(int mflLeagueId, int week, String serverId);
 
 
-    List<DraftPick> getDraftPicks(int mflLeagueId, String serverId, int year);
+
+	WeeklyResultsWrapper getWeeklyResults(int mflLeagueId, int week, String serverId, int year);
+
+	/**
+	 * Gets the standings.
+	 *
+	 * @param mflLeagueId the mfl league id
+	 * @param serverId the server id
+	 * @param year the year
+	 * @return the standings
+	 */
+	List<Standings> getStandings(int mflLeagueId, String serverId, int year);
+
+
+	/**
+	 * Gets the draft picks.
+	 *
+	 * @param mflLeagueId the mfl league id
+	 * @param serverId the server id
+	 * @param year the year
+	 * @return the draft picks
+	 */
+	List<DraftPick> getDraftPicks(int mflLeagueId, String serverId, int year);
 }
--- src/main/java/org/tiltedwindmills/fantasy/mfl/services/impl/JsonLeagueServiceImpl.java
+++ src/main/java/org/tiltedwindmills/fantasy/mfl/services/impl/JsonLeagueServiceImpl.java
@@ -32,164 +32,164 @@ import retrofit.RetrofitError;
 @Service
 public final class JsonLeagueServiceImpl extends AbstractJsonServiceImpl implements LeagueService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(JsonLeagueServiceImpl.class);
+	private static final Logger LOG = LoggerFactory.getLogger(JsonLeagueServiceImpl.class);
 
-    // service names for validation routines
-    private static final String WEEKLY_RESULTS_SERVICE = "weekly results";
+	// service names for validation routines
+	private static final String WEEKLY_RESULTS_SERVICE = "weekly results";
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#searchLeagues(java.lang.String, int)
-     */
-    @Override
-    public List<LeagueSearchResult> searchLeagues(final String email, final int year) {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#searchLeagues(java.lang.String, int)
+	 */
+	@Override
+	public List<LeagueSearchResult> searchLeagues(final String email, final int year) {
 
-        LOG.info("Searching {} leagues with email: {}", year, email);
+		LOG.info("Searching {} leagues with email: {}", year, email);
 
-        // no server ID needed for searching.
-        final String serverId = "";
+		// no server ID needed for searching.
+		final String serverId = "";
 
-        final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-        final SearchLeagueResponse searchLeagueResponse = leagueExport.searchLeaguesByEmail(email, year);
+		final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+		final SearchLeagueResponse searchLeagueResponse = leagueExport.searchLeaguesByEmail(email, year);
 
-        final LeagueSearchResultWrapper leagueWrapper = searchLeagueResponse.getLeagues();
-        return leagueWrapper.getLeagues();
-    }
+		final LeagueSearchResultWrapper leagueWrapper = searchLeagueResponse.getLeagues();
+		return leagueWrapper.getLeagues();
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getLeague(int, java.lang.String)
-     */
-    @Override
-    public League getLeague(final int mflLeagueId, final String serverId) {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getLeague(int, java.lang.String)
+	 */
+	@Override
+	public League getLeague(final int mflLeagueId, final String serverId) {
 
-        LOG.info("Retrieving {} league with ID: {}", CURRENT_YEAR, mflLeagueId);
-        final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-        final LeagueResponse leagueResponse = leagueExport.getLeague(mflLeagueId, CURRENT_YEAR);
+		LOG.info("Retrieving {} league with ID: {}", CURRENT_YEAR, mflLeagueId);
+		final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+		final LeagueResponse leagueResponse = leagueExport.getLeague(mflLeagueId, CURRENT_YEAR);
 
-        return leagueResponse.getLeague();
-    }
+		return leagueResponse.getLeague();
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getLeague(int, java.lang.String, java.lang.String,
-     * java.lang.String)
-     */
-    @Override
-    public League getLeague(final int mflLeagueId, final String franchiseId, final String password,
-            final String serverId) {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getLeague(int, java.lang.String, java.lang.String,
+	 * java.lang.String)
+	 */
+	@Override
+	public League getLeague(final int mflLeagueId, final String franchiseId, final String password,
+			final String serverId) {
 
-        LOG.info("Retrieving full details for {} league with ID: {}", CURRENT_YEAR, mflLeagueId);
-        final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-        final LeagueResponse leagueResponse = leagueExport.getLeague(mflLeagueId, franchiseId, password, CURRENT_YEAR);
+		LOG.info("Retrieving full details for {} league with ID: {}", CURRENT_YEAR, mflLeagueId);
+		final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+		final LeagueResponse leagueResponse = leagueExport.getLeague(mflLeagueId, franchiseId, password, CURRENT_YEAR);
 
-        return leagueResponse.getLeague();
-    }
+		return leagueResponse.getLeague();
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getLiveScoring(int, int, java.lang.String)
-     */
-    @Override
-    public LiveScoringWrapper getLiveScoring(final int mflLeagueId, final int week, final String serverId) {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getLiveScoring(int, int, java.lang.String)
+	 */
+	@Override
+	public LiveScoringWrapper getLiveScoring(final int mflLeagueId, final int week, final String serverId) {
 
-        LOG.info("Loading league {} scoring for {} week {}", mflLeagueId, CURRENT_YEAR, week);
+		LOG.info("Loading league {} scoring for {} week {}", mflLeagueId, CURRENT_YEAR, week);
 
-        final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-        final LiveScoringResponse liveScoringResponse = leagueExport.getLiveScoring(mflLeagueId, week, CURRENT_YEAR);
+		final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+		final LiveScoringResponse liveScoringResponse = leagueExport.getLiveScoring(mflLeagueId, week, CURRENT_YEAR);
 
-        // iterate the response object to get rid of the weird issue where MFL returns two teams on bye in a matchup
-        for (final Iterator<Matchup> i = liveScoringResponse.getLiveScoring().getMatchups().iterator(); i.hasNext();) {
+		// iterate the response object to get rid of the weird issue where MFL returns two teams on bye in a matchup
+		for (final Iterator<Matchup> i = liveScoringResponse.getLiveScoring().getMatchups().iterator(); i.hasNext();) {
 
-            final Matchup matchup = i.next();
+			final Matchup matchup = i.next();
 
-            // TODO : document this behavior
+			// TODO : document this behavior
 
-            boolean foundLegitFranchise = false;
-            for (TeamScoringDetails tsd : matchup.getTeams()) {
+			boolean foundLegitFranchise = false;
+			for (TeamScoringDetails tsd : matchup.getTeams()) {
 
-                // if "BYE" is the franchise ID, its no good.
-                if (!"BYE".equals(tsd.getFranchiseId())) {
-                    foundLegitFranchise = true;
-                    break;
-                }
-            }
+				// if "BYE" is the franchise ID, its no good.
+				if (!"BYE".equals(tsd.getFranchiseId())) {
+					foundLegitFranchise = true;
+					break;
+				}
+			}
 
-            // if we failed to find a real team in the list, kill it.
-            if (!foundLegitFranchise) {
-                i.remove();
-            }
-        }
+			// if we failed to find a real team in the list, kill it.
+			if (!foundLegitFranchise) {
+				i.remove();
+			}
+		}
 
-        return liveScoringResponse.getLiveScoring();
-    }
+		return liveScoringResponse.getLiveScoring();
+	}
 
-    @Override
-    public WeeklyResultsWrapper getWeeklyResults(
-            final int mflLeagueId, final int week, final String serverId, final int year) {
+	@Override
+	public WeeklyResultsWrapper getWeeklyResults(
+			final int mflLeagueId, final int week, final String serverId, final int year) {
 
-        validateLeagueId(mflLeagueId, WEEKLY_RESULTS_SERVICE);
-        validateServerId(serverId, WEEKLY_RESULTS_SERVICE);
-        validateYear(year, WEEKLY_RESULTS_SERVICE);
+		validateLeagueId(mflLeagueId, WEEKLY_RESULTS_SERVICE);
+		validateServerId(serverId, WEEKLY_RESULTS_SERVICE);
+		validateYear(year, WEEKLY_RESULTS_SERVICE);
 
-        try {
-            final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-            final WeeklyResultsResponse response = leagueExport.getWeeklyResults(mflLeagueId, week, year);
+		try {
+			final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+			final WeeklyResultsResponse response = leagueExport.getWeeklyResults(mflLeagueId, week, year);
 
-            if (response == null) {
-                LOG.error("Invalid response retrieving weekly results for week {} in league {}.", week, mflLeagueId);
-                throw new MFLServiceException("Invalid response retrieving weekly results");
-            }
+			if (response == null) {
+				LOG.error("Invalid response retrieving weekly results for week {} in league {}.", week, mflLeagueId);
+				throw new MFLServiceException("Invalid response retrieving weekly results");
+			}
 
-            return response.getWeeklyResults();
+			return response.getWeeklyResults();
 
-        } catch (RetrofitError e) {
+		} catch (RetrofitError e) {
 
-            LOG.error("Error retrieving {} league {} weekly results data for week {} : {}",
-                    year, mflLeagueId, week, e.getMessage());
+			LOG.error("Error retrieving {} league {} weekly results data for week {} : {}",
+					year, mflLeagueId, week, e.getMessage());
 
-            throw new MFLServiceException("Error retrieving weekly results data.", e);
-        }
-    }
+			throw new MFLServiceException("Error retrieving weekly results data.", e);
+		}
+	}
 
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getStandings(int, java.lang.String, int)
-     */
-    @Override
-    public List<Standings> getStandings(final int mflLeagueId, final String serverId, final int year) {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.tiltedwindmills.fantasy.mfl.services.LeagueService#getStandings(int, java.lang.String, int)
+	 */
+	@Override
+	public List<Standings> getStandings(final int mflLeagueId, final String serverId, final int year) {
 
-        LOG.info("Loading league {} standings for {}", mflLeagueId, CURRENT_YEAR);
+		LOG.info("Loading league {} standings for {}", mflLeagueId, CURRENT_YEAR);
 
-        final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-        final StandingsResponse standingsResponse = leagueExport.getStandings(mflLeagueId, CURRENT_YEAR);
+		final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+		final StandingsResponse standingsResponse = leagueExport.getStandings(mflLeagueId, CURRENT_YEAR);
 
-        if (standingsResponse != null && standingsResponse.getWrapper() != null) {
-            return standingsResponse.getWrapper().getStandings();
-        }
+		if (standingsResponse != null && standingsResponse.getWrapper() != null) {
+			return standingsResponse.getWrapper().getStandings();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public List<DraftPick> getDraftPicks(final int mflLeagueId, final String serverId, final int year) {
+	@Override
+	public List<DraftPick> getDraftPicks(final int mflLeagueId, final String serverId, final int year) {
 
-        LOG.info("Loading league {} draft picks for {}", mflLeagueId, CURRENT_YEAR);
+		LOG.info("Loading league {} draft picks for {}", mflLeagueId, year);
 
-        final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
-        final DraftResultsResponse draftResultsResponse = leagueExport.getDraftResults(mflLeagueId, CURRENT_YEAR);
+		final MflLeagueExport leagueExport = getRestAdapter(serverId).create(MflLeagueExport.class);
+		final DraftResultsResponse draftResultsResponse = leagueExport.getDraftResults(mflLeagueId, year);
 
-        /* XXX : lots of NPE issues here */
-        if (draftResultsResponse != null && draftResultsResponse.getDraftResults() != null) {
-            return draftResultsResponse.getDraftResults().getDrafts().get(0).getPicks();
-        }
+		/* XXX : lots of NPE issues here */
+		if (draftResultsResponse != null && draftResultsResponse.getDraftResults() != null) {
+			return draftResultsResponse.getDraftResults().getDrafts().get(0).getPicks();
+		}
 
-        return null;
-    }
+		return null;
+	}
 }
